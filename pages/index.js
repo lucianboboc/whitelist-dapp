c60ae925cafee338db7ca70f8c6ca7f5f0ee0699
Lucian Boboc <info@lucianboboc.com>
2022-08-20 02:26:45 +0200

    fixed button state

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,21 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Web3Modal from 'web3modal'
-import { providers, Contract } from 'ethers'
-import { useEffect, useRef, useState } from 'react'
-import { WHITELIST_CONTRACT_ADDRESS, abi } from '../constants'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Web3Modal from "web3modal";
+import { providers, Contract } from "ethers";
+import { useEffect, useRef, useState } from "react";
+import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
 
-const Home = () => {
+export default function Home() {
   // walletConnected keep track of whether the user's wallet is connected or not
   const [walletConnected, setWalletConnected] = useState(false);
-
   // joinedWhitelist keeps track of whether the current metamask address has joined the Whitelist or not
   const [joinedWhitelist, setJoinedWhitelist] = useState(false);
-
   // loading is set to true when we are waiting for a transaction to get mined
   const [loading, setLoading] = useState(false);
-
   // numberOfWhitelisted tracks the number of addresses's whitelisted
   const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0);
-
   // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
   const web3ModalRef = useRef();
-
 
   /**
    * Returns a Provider or Signer object representing the Ethereum RPC with or without the
@@ -34,7 +29,7 @@ const Home = () => {
    *
    * @param {*} needSigner - True if you need the signer, default false otherwise
    */
-  const getProviderOrSigner = async (needSigner = true) => {
+  const getProviderOrSigner = async (needSigner = false) => {
     // Connect to Metamask
     // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
     const provider = await web3ModalRef.current.connect();
@@ -44,17 +39,15 @@ const Home = () => {
     const { chainId } = await web3Provider.getNetwork();
     if (chainId !== 4) {
       window.alert("Change the network to Rinkeby");
-      throw new Error("Change the network to Rinkeby");
+      throw new Error("Change network to Rinkeby");
     }
 
     if (needSigner) {
       const signer = web3Provider.getSigner();
       return signer;
     }
-
     return web3Provider;
-  }
-
+  };
 
   /**
    * addAddressToWhitelist: Adds the current connected address to the whitelist
@@ -63,7 +56,6 @@ const Home = () => {
     try {
       // We need a Signer here since this is a 'write' transaction.
       const signer = await getProviderOrSigner(true);
-      console.log("signer created");
       // Create a new instance of the Contract with a Signer, which allows
       // update methods
       const whitelistContract = new Contract(
@@ -71,23 +63,19 @@ const Home = () => {
         abi,
         signer
       );
-      console.log("whitelistContract created");
-      console.log("addAddressToWhitelist called");
       // call the addAddressToWhitelist from the contract
-      const tx = whitelistContract.addAddressToWhitelist();
+      const tx = await whitelistContract.addAddressToWhitelist();
       setLoading(true);
-      console.log("addAddressToWhitelist executed");
       // wait for the transaction to get mined
       await tx.wait();
       setLoading(false);
-      console.log("addAddressToWhitelist ex finished");
       // get the updated number of addresses in the whitelist
       await getNumberOfWhitelisted();
       setJoinedWhitelist(true);
     } catch (err) {
-      console.log(err);
+      console.error(err);
     }
-  }
+  };
 
   /**
    * getNumberOfWhitelisted:  gets the number of whitelisted addresses
@@ -96,8 +84,7 @@ const Home = () => {
     try {
       // Get the provider from web3Modal, which in our case is MetaMask
       // No need for the Signer here, as we are only reading state from the blockchain
-      const provider = getProviderOrSigner();
-
+      const provider = await getProviderOrSigner();
       // We connect to the Contract using a Provider, so we will only
       // have read-only access to the Contract
       const whitelistContract = new Contract(
@@ -105,14 +92,13 @@ const Home = () => {
         abi,
         provider
       );
-
       // call the numAddressesWhitelisted from the contract
       const _numberOfWhitelisted = await whitelistContract.numAddressesWhitelisted();
       setNumberOfWhitelisted(_numberOfWhitelisted);
     } catch (err) {
-      console.log(err);
+      console.error(err);
     }
-  }
+  };
 
   /**
    * checkIfAddressInWhitelist: Checks if the address is in whitelist
@@ -128,20 +114,17 @@ const Home = () => {
         abi,
         signer
       );
-
       // Get the address associated to the signer which is connected to  MetaMask
       const address = await signer.getAddress();
-
       // call the whitelistedAddresses from the contract
-      const _joinedWhitelist = whitelistContract.whitelistedAddress(
+      const _joinedWhitelist = await whitelistContract.whitelistedAddresses(
         address
       );
       setJoinedWhitelist(_joinedWhitelist);
     } catch (err) {
-      console.log(err);
+      console.error(err);
     }
-  }
-
+  };
 
   /*
     connectWallet: Connects the MetaMask wallet
@@ -156,13 +139,12 @@ const Home = () => {
       checkIfAddressInWhitelist();
       getNumberOfWhitelisted();
     } catch (err) {
-      console.log(err);
+      console.error(err);
     }
-  }
-
+  };
 
   /*
-  renderButton: Returns a button based on the state of the dapp
+    renderButton: Returns a button based on the state of the dapp
   */
   const renderButton = () => {
     if (walletConnected) {
@@ -188,8 +170,7 @@ const Home = () => {
         </button>
       );
     }
-  }
-
+  };
 
   // useEffects are used to react to changes in state of the website
   // The array at the end of function call represents what state changes will trigger this effect
@@ -230,11 +211,10 @@ const Home = () => {
           <img className={styles.image} src="./crypto-devs.svg" />
         </div>
       </div>
-      <footer className={styles.image} src="./crypto-devs.svg">
+
+      <footer className={styles.footer}>
         Made with &#10084; by Crypto Devs
       </footer>
     </div>
   );
 }
-
-export default Home
